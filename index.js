6c3f91e065d772e283d9d517dc57b781fb71f253
unknown <lizzybumpers11@gmail.com>
2023-11-24 18:43:04 -0600

    trying to get class Square to make a square

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Triangle, Circle, Square } = require("./lib/shapes.js")
+const { Triangle, Circle, Square } = require("./lib/shape.js")
 
 const questions = [
     {
